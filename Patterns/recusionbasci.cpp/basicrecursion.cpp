d7d119f5e764f89ed96bcc1bde09b5c278c88d48
ayushcode01 <akchauhan29feb@gmail.com>
2024-05-18 15:06:37 +0530

    print1ton

--- Patterns/recusionbasci.cpp/basicrecursion.cpp
+++ Patterns/recusionbasci.cpp/basicrecursion.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
-void greet(){
-    cout<<"gud morng"<<endl;
-    greet();
+void print(int n){
+    if(n==0) return;
+    cout<<n<<endl;
+    print(n-1);
 }
 int main (){
-    greet();
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    print(n);
 }
